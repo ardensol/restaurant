4d7400eadc5d518db7804922a39e563e930a349e
carlos <csolorio05@gmail.com>
2015-01-17 19:30:24 -0500

    stuff

---
+++ app/controllers/spree/staticpages_controller.rb
@@ -0,0 +1,26 @@
+class Spree::StaticpagesController < Spree::StoreController
+  
+  def aboutus
+  end 
+
+  def delivery
+  end
+
+  def privacy
+  end
+
+  def terms
+  end
+
+  def contact
+  end
+
+  def returns
+  end
+
+  def affiliates
+  end
+  
+end
+
+
