f2eafd5faba7c0bf48fe302925ef6db032195e5c
carlos <csolorio05@gmail.com>
2015-02-04 13:29:56 -0500

    updated aws upload

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,34 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+  attachment_config = {
+    s3_credentials: {
+      access_key_id: ENV.fetch("S3_ACCESS_KEY"),
+      secret_access_key: ENV.fetch("S3_SECRET"),
+      bucket: ENV.fetch("S3_BUCKET"),
+    },
+
+    storage:        :s3,
+    s3_headers:     { "Cache-Control" => "max-age=31557600" },
+    s3_protocol:    "https",
+ 	bucket:         ENV.fetch("S3_BUCKET"),
+
+    styles: {
+      mini:     "95x95>",
+      small:    "100x100>",
+      product:  "555x555>",
+      large:    "360x360>",
+      xlarge:   "555x555>"
+    },
+
+    path:          ":rails_root/public/spree/products/:id/:style/:basename.:extension",
+    default_url:   "/spree/products/:id/:style/:basename.:extension",
+    default_style: "product",
+  }
+
+  attachment_config.each do |key, value|
+    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+  end
 end
 
 Spree.user_class = "Spree::User"
