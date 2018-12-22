6d76239929271f87c4c6f4726be7205a17347616
carlos s <csolorio05@gmail.com>
2018-12-22 11:31:33 -0700

    fix

--- config/initializers/paperclip_fixes.rb
+++ config/initializers/paperclip_fixes.rb
@@ -5,16 +5,16 @@ module VerifyMimeType
   # thinks it is. Sometimes images uploaded from the web or mobile are determined to have
   # `application/octet-stream` as their content type, causing silent validation errors that
   # break counter caches.
-  def verify_mime_type(instance, attachment_name='image')
+  def verify_mime_type(instance, attachment_name='attachment')
     interpolated_mime_type = instance.send("#{attachment_name}_content_type".to_sym)
+    return interpolated_mime_type unless instance.s3_url
     actual_mime_type = nil
-
     begin
       Timeout::timeout(3) do
         retries ||= 0
 
         if interpolated_mime_type == 'application/octet-stream'
-          temp_file = open(instance.send(attachment_name.to_sym).url.prepend('https:'))
+          temp_file = URI.parse(instance.s3_url)
           attachment_path = temp_file.path
           temp_file && temp_file.close
           actual_mime_type = `file --b --mime-type '#{attachment_path}'`.strip
