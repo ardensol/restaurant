8e45e984efccea339f3b360f0154e71320420946
carlos s <csolorio05@gmail.com>
2018-12-22 11:50:35 -0700

    test

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -3,20 +3,4 @@ Spree::Image.class_eval do
 
   p "LOADED IMAGE CLASS EVAL"
 
-  has_attached_file :attachment,
-                    validate_media_type: false,
-                    styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
-                    default_style: :product,
-                    url: '/spree/products/:id/:style/:basename.:extension',
-                    path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
-                    convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
-
-  do_not_validate_attachment_file_type :attachment
-  # validates_attachment :attachment,
-  #                      :presence => true,
-  #                      :content_type => { :content_type => %w(application/octet-stream image/jpeg image/jpg image/png image/gif) }
-  #
-  def fake_method
-    p 'test'
-  end
 end
