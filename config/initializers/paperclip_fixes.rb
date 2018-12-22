d2371694f3d3704383a1795a41337875a7975f02
carlos s <csolorio05@gmail.com>
2018-12-21 20:39:04 -0700

    test

--- config/initializers/paperclip_fixes.rb
+++ config/initializers/paperclip_fixes.rb
@@ -2,13 +2,11 @@ require 'paperclip/media_type_spoof_detector'
 
 module Paperclip
   class MediaTypeSpoofDetector
-    old_spoofed = instance_method(:spoofed?)
+    def type_from_file_command
+      begin
+        Paperclip.run("file", "-b --mime :file", :file => @file.path)
+      rescue
 
-    define_method(:spoofed?) do
-      if supplied_file_content_types.count > 0
-        old_spoofed.bind(self).()
-      else
-        false
       end
     end
   end
