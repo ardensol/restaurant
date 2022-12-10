8236cfb2cbdd4dd26cd99a028d659da0025a9760
carlos s <csolorio05@gmail.com>
2022-12-09 21:43:33 -0700

    fix

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,5 @@
 class WebhooksController < ApplicationController
   def sync
-    Inventories::Sync.new.execute_single(params['inventory'])
+    Inventories::Sync.new.execute_single(params)
   end
 end
